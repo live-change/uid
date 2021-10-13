0c22a16aadccd907d4a59d82ab0ae4185e61ae54
Michał Łaszczewski <michal@laszczewski.com>
2021-10-13 13:08:58 +0200

    hashCode function fix

--- index.js
+++ index.js
@@ -60,13 +60,13 @@ function randomString(length = 8) {
 
 function hashCode(str) {
   let hash = 0
-  if(this.length == 0) return hash
+  if(str.length == 0) return hash
   for(let i = 0; i < str.length; i++) {
     const char = str.charCodeAt(i)
     hash = ((hash << 5) - hash) + char
-    hash = hash & hash // Convert to 32bit integer
+    hash = hash | 0
   }
-  return hash
+  return Math.abs(hash)
 }
 
 function uidGenerator(fingerprint = randomString(4), numberLength = 0) {
